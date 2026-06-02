37206fb5bd99d7b357206cf4e8d544ce6ff15eca
msamblf <matiasleonel.samblancatfredriksson@emeal.nttdata.com>
2026-06-02 12:57:00 +0200

    Changed example to user native ODBC / JDBC

--- microsoft-fabric/integracio_per_consultar_dades/exemples/jdbc/exemple_jdbc.py
+++ microsoft-fabric/integracio_per_consultar_dades/exemples/jdbc/exemple_jdbc.py
@@ -2,40 +2,36 @@
 Example: Connecting to Microsoft Fabric Lakehouse SQL Endpoint via JDBC
 """
 
-import jaydebeapi
-from azure.identity import InteractiveBrowserCredential
+import pyodbc
+from azure.identity import ClientSecretCredential
 import struct
 
 # --------- AUTHENTICATION ---------
-SCOPE = "https://database.windows.net/.default"
-TENANT_ID = "<TENANT_ID>"                       # e.g., "contoso.onmicrosoft.com" or GUID
-SERVER = "<SERVER>"                             # e.g. xxxxxx-s12345.datawarehouse.fabric.microsoft.com
-DATABASE = "<LAKEHOUSE_NAME>"                   # e.g. lakehouse_gold
+
 DRIVER_CLASS = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
-JDBC_URL = f"jdbc:sqlserver://{SERVER}:1433;database={DATABASE};encrypt=true;trustServerCertificate=false;"
-JAR_FILE = "mssql-jdbc-12.6.1.jre11.jar"        # Adjust version as needed
+CONN_STR = (
+    "DRIVER={ODBC Driver 18 for SQL Server};"
+    "SERVER=#SERVER#,1433;"
+    "DATABASE=#DATABASE#;"
+    "Authentication=ActiveDirectoryServicePrincipal;"
+    "UID=#SERVICE_PRINCIPAL_CLIENT_ID#;"
+    "PWD=#SERVICE_PRINCIPAL_CLIENT_SECRET#;"
+    "Encrypt=yes;"
+    "TrustServerCertificate=no;"
+)
 
-# Get token
-cred = InteractiveBrowserCredential(tenant_id=TENANT_ID)
-token = cred.get_token(SCOPE)
 
-token_bytes = token.token.encode("utf-16-le")
-packed_token = struct.pack(f"<I{len(token_bytes)}s", len(token_bytes), token_bytes)
-
-# JDBC connection properties
-properties = {
-    "accessToken": token.token
-}
 
 # --------- CONNECT ---------
 try:
-    conn = jaydebeapi.connect(DRIVER_CLASS, JDBC_URL, properties, JAR_FILE)
-    curs = conn.cursor()
-    curs.execute("SELECT TOP 10 * FROM <schema>.<table>;")
-    rows = curs.fetchall()
-    for r in rows:
-        print(r)
-    curs.close()
+    conn = pyodbc.connect(CONN_STR)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT TOP 10 * FROM aoc_valid.fac_autenticacio;")
+    print(cursor.fetchone())
+
+    cursor.close()
     conn.close()
+
 except Exception as e:
     print("Connection failed:", e)
