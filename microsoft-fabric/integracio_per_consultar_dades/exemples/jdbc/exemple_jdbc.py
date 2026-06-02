5d79739bedb6b183aeb0c3eb2feee8c4366bd7b6
msamblf <matiasleonel.samblancatfredriksson@emeal.nttdata.com>
2026-06-02 12:58:24 +0200

    added comments

--- microsoft-fabric/integracio_per_consultar_dades/exemples/jdbc/exemple_jdbc.py
+++ microsoft-fabric/integracio_per_consultar_dades/exemples/jdbc/exemple_jdbc.py
@@ -11,11 +11,11 @@ import struct
 DRIVER_CLASS = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
 CONN_STR = (
     "DRIVER={ODBC Driver 18 for SQL Server};"
-    "SERVER=#SERVER#,1433;"
-    "DATABASE=#DATABASE#;"
+    "SERVER=#SERVER#,1433;"                     ## Update with your server name
+    "DATABASE=#DATABASE#;"                      ## e.g., lakehouse_gold
     "Authentication=ActiveDirectoryServicePrincipal;"
-    "UID=#SERVICE_PRINCIPAL_CLIENT_ID#;"
-    "PWD=#SERVICE_PRINCIPAL_CLIENT_SECRET#;"
+    "UID=#SERVICE_PRINCIPAL_CLIENT_ID#;"        ## Update with your service principal client ID 
+    "PWD=#SERVICE_PRINCIPAL_CLIENT_SECRET#;"    ## Update with your service principal client secret
     "Encrypt=yes;"
     "TrustServerCertificate=no;"
 )
